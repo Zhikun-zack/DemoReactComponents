fe6fb2540fb6656d44f3f884c01e28457b7a8213
Zack <hzkzqxx@163.com>
2021-04-12 07:27:37 -0700

    init

---
+++ src/Carousel.js
@@ -0,0 +1,16 @@
+import {Component} from "react";
+
+class Carousel extends Component{
+    render(){
+        return(
+            <div>
+                <div className="title">
+                    <h1>Carousel</h1>
+                </div>
+                <div>Carousel</div>
+            </div>
+        )
+        
+    }
+}
+export default Carousel;
